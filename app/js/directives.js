7e635b4aa402094c418bbce48c1c99a80f67ec74
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-11-25 13:28:09 -0500

    Allow for client side vote reversal.

--- app/js/directives.js
+++ app/js/directives.js
@@ -11,35 +11,76 @@ p2pDirectives.directive('uservote', ['$routeParams', 'Question', function ($rout
              'answer_id' : '=data'},
     templateUrl: '../partials/voting.html',
     link : function(scope, element, attrs) {
+
+      var voteQuestion = function(direction) {
+        Question.put({questionId: $routeParams.questionId},
+                     {"question": {"vote": direction}});
+      };
+
+      var voteAnswer = function(direction) {
+        Question.put({questionId: $routeParams.questionId},
+                     {"answer": {"vote": direction,
+                                 "answer_id": scope.answer_id}});
+      };
+
       element.find('i').eq(0).on('click', function(event) {
-        if(scope.answer_id) {
-          Question.put({questionId: $routeParams.questionId},
-                       {"answer": {"vote": "up",
-                                  "answer_id": scope.answer_id}});
+        if (!scope.upvoted) {
+          if (scope.answer_id) {
+            voteAnswer("up");
+          }
+          else {
+            voteQuestion("up");
+          }
+
+          scope.upvotedStyle = {'background-color':'green'};
+          scope.downvotedStyle = {'background-color':''};
+          scope.votes = scope.votes + 1;
+          scope.upvoted = true;
+
         }
         else {
-          Question.put({questionId: $routeParams.questionId},
-                       {"question": {"vote": "up"}});
-        }
+          scope.upvoted = false;
+          scope.votes = scope.votes - 1;
+          scope.upvotedStyle = {'background-color':''};
 
-        scope.upvotedStyle = {'background-color':'green'};
-        scope.votes = scope.votes + 1;
+          if (scope.answer_id) {
+            voteAnswer("down");
+          }
+          else {
+            voteQuestion("down");
+          }
+        }
       });
 
       element.find('i').eq(1).on('click', function(event) {
-        if(scope.answer_id) {
-          Question.put({questionId: $routeParams.questionId},
-                       {"answer": {"vote": "down",
-                                   "answer_id": scope.answer_id}});
+        if (!scope.downvoted) {
+          if (scope.answer_id) {
+            voteAnswer("down");
+          }
+          else {
+            voteQuestion("down");
+          }
+
+          scope.downvotedStyle = {'background-color':'red'};
+          scope.upvotedStyle = {'background-color':''};
+          scope.votes = scope.votes - 1;
+          scope.downvoted = true;
         }
         else {
-          Question.put({questionId: $routeParams.questionId},
-                       {"question": {"vote": "down"}});
-        }
+          scope.downvoted = false;
+          scope.votes = scope.votes + 1;
+          scope.downvotedStyle = {'background-color':''};
 
-        scope.downvotedStyle = {'background-color':'red'};
-        scope.votes = scope.votes - 1;
-      });
+          if (scope.answer_id) {
+            voteAnswer("up");
+
+          }
+          else {
+            voteQuestion("up");
+          }
+        }
+      }
+                                );
 
     }
   };
